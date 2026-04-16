510b8982860aa8effd24a1f6f66203e6107e881f
Ajay <ajay.goswami@coderower.com>
2026-04-16 13:58:50 +0530

    feat: remove border from header sections in multiple pages for a cleaner UI

--- src/pages/master/other/AttUploadSite.tsx
+++ src/pages/master/other/AttUploadSite.tsx
@@ -34,7 +34,7 @@ const AttUploadSitePage: React.FC = () => {
 
   return (
     <div className="space-y-4">
-      <div className="flex items-center justify-between border-b pb-3">
+      <div className="flex items-center justify-between pb-3">
         <Title level={4} className="!mb-0">Attendance Upload Company Site Master</Title>
       </div>
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
--- src/pages/master/other/Shift.tsx
+++ src/pages/master/other/Shift.tsx
@@ -85,7 +85,7 @@ const ShiftPage: React.FC = () => {
 
   return (
     <div className="space-y-4">
-      <div className="flex items-center justify-between border-b pb-3">
+      <div className="flex items-center justify-between pb-3">
         <Title level={4} className="!mb-0">{editingId ? 'Edit Shift' : 'Shift'}</Title>
       </div>
 
